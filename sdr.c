9ea000710e4794e2951761b3b06f9236982118c3
Adam Walker <adam.walker@nicta.com.au>
2014-03-13 12:35:42 -0400

    resampling with real coefficients

--- sdr.c
+++ sdr.c
@@ -210,3 +210,47 @@ int resample_crossbuf_c(int interpolation, int decimation, int coeff_size, doubl
     return filter_offset;
 }
 
+int resample_onebuf_r(int interpolation, int decimation, int coeff_size, double *coeffs, int filter_offset, int buf_size, double *in_buf, double *out_buf){
+    int j, k, l;
+    int input_offset = 0;
+    for(k=0; k<buf_size; k++) {
+        double accum = 0;
+
+        for(l=0, j=filter_offset; j<coeff_size; l++, j+=interpolation) {
+            accum += in_buf[input_offset + l] * coeffs[j];
+        }
+
+        int filter_offset_new  = interpolation - 1 - (decimation - filter_offset - 1) % interpolation;
+        input_offset          += (decimation - filter_offset - 1) / interpolation + 1; 
+        filter_offset          = filter_offset_new;
+
+        out_buf[k] = accum;
+    }
+
+    return filter_offset;
+}
+
+int resample_crossbuf_r(int interpolation, int decimation, int coeff_size, double *coeffs, int filter_offset, int remaining_input, int buf_size, double *last_buf, double *this_buf, double *out_buf){
+
+    int j, k, l;
+    int input_offset = 0;
+    for(k=0; k<buf_size; k++) {
+        double accum = 0;
+
+        for(l=0, j=filter_offset; input_offset + l < remaining_input; l++, j+=interpolation) {
+            accum += last_buf[input_offset + l] * coeffs[j];
+        }
+
+        for(; j<coeff_size; j+=interpolation) {
+            accum += this_buf[input_offset + l - remaining_input] * coeffs[j];
+        }
+
+        int filter_offset_new = interpolation - 1 - (decimation - filter_offset - 1) % interpolation;
+        input_offset         += (decimation - filter_offset - 1) / interpolation + 1;
+        filter_offset         = filter_offset_new;
+
+        out_buf[k] = accum;
+    }
+    return filter_offset;
+}
+
