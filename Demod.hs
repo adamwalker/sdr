65f77a450122a44b3e76a80600ba587c4142777b
Adam Walker <adam.walker@nicta.com.au>
2014-03-17 20:08:29 -0400

    make fmDemod a pipe

--- Demod.hs
+++ Demod.hs
@@ -6,17 +6,25 @@ import Foreign.ForeignPtr
 import Foreign.C.Types
 import Data.Complex
 import Foreign.Marshal.Alloc
+import Foreign.Marshal.Array
+
+import Pipes
 
 foreign import ccall unsafe "fmDemod"
     c_fmDemod :: CInt -> Ptr (Complex CDouble) -> Ptr (Complex CDouble) -> Ptr CDouble -> IO ()
 
-fmDemod :: Int -> ForeignPtr (Complex CDouble) -> IO (ForeignPtr CDouble)
-fmDemod samples ina = do
-    out <- mallocForeignPtrArray samples
-    withForeignPtr ina $ \ip -> 
-        withForeignPtr out $ \op -> 
-        alloca $ \sp -> do
-            poke sp (fromIntegral 0)
-            c_fmDemod (fromIntegral samples) sp ip op
-    return out
+fmDemod :: Int -> Pipe (ForeignPtr (Complex CDouble)) (ForeignPtr CDouble) IO ()
+fmDemod samples = fmDemod' 0
+    where
+    fmDemod' lastVal = do
+        ina <- await
+        out <- lift $ mallocForeignPtrArray samples
+        last <- lift $ withForeignPtr ina $ \ip -> do
+            withForeignPtr out $ \op -> 
+                alloca $ \sp -> do
+                    poke sp lastVal
+                    c_fmDemod (fromIntegral samples) sp ip op
+            peek $ advancePtr ip (samples - 1)
+        yield out
+        fmDemod' last
 
