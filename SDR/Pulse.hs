74f6df1b49cbe5197a7802a79bb7bd72c63d6b06
Adam Walker <adamwalker10@gmail.com>
2014-06-14 23:28:50 -0400

    vectors in pulse sink

--- SDR/Pulse.hs
+++ SDR/Pulse.hs
@@ -7,23 +7,25 @@ import Control.Monad
 import Control.Concurrent
 import Data.ByteString.Internal
 
+import Data.Vector.Storable as VS
+
 import Sound.Pulse.Simple
 
 import Pipes
 import Pipes.Concurrent
 
-pulseAudioSink :: Int -> IO (Consumer (ForeignPtr CFloat) IO ())
-pulseAudioSink samples = do
+pulseAudioSink :: IO (Consumer (VS.Vector CFloat) IO ())
+pulseAudioSink = do
     (output, input) <- spawn Unbounded
-    doIt <- doPulse samples
+    doIt <- doPulse 
     forkOS $ runEffect $ fromInput input >-> doIt
     return $ toOutput output
 
-doPulse :: Int -> IO (Consumer (ForeignPtr CFloat) IO ())
-doPulse samples = do
+doPulse :: IO (Consumer (VS.Vector CFloat) IO ())
+doPulse = do
     s <- simpleNew Nothing "example" Play Nothing "this is an example application" (SampleSpec (F32 LittleEndian) 48000 1) Nothing Nothing
     return $ forever $ do
         buf <- await
         lift $ do
-            --simpleDrain s
-            simpleWriteRaw s (PS (castForeignPtr buf) 0 (samples * 4))
+            let (fp, offset, length) = VS.unsafeToForeignPtr buf
+            simpleWriteRaw s (PS (castForeignPtr fp) offset (length * 4))
