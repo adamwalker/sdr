ec070d32463c55c0a77201733d3386c66f7b1215
Adam Walker <adamwalker10@gmail.com>
2014-06-14 22:43:15 -0400

    changes to plotting

--- SDR/Plot.hs
+++ SDR/Plot.hs
@@ -8,13 +8,17 @@ import Foreign.Marshal.Array
 import Foreign.ForeignPtr
 import Foreign.Storable.Complex
 
+import Graphics.Rendering.OpenGL
+
 import Pipes
 
-import DynamicGraph.SimpleLine
+import Graphics.DynamicGraph.SimpleLine  as SL
+import Graphics.DynamicGraph.TextureLine as TL
+import Graphics.DynamicGraph.Waterfall   as WF
 
-plot :: Int -> CFloat -> EitherT String IO (Consumer (ForeignPtr (Complex CDouble)) IO ())
-plot samples gain = do
-    graphFunc <- graph
+plotSimple :: Int -> Int -> Int -> CFloat -> EitherT String IO (Consumer (ForeignPtr (Complex CDouble)) IO ())
+plotSimple width height samples gain = do
+    graphFunc <- SL.graph width height samples
     let xCoords = take samples $ iterate (+ (2 / fromIntegral samples)) (-1)
     return $ forever $ do
         dat <- await
@@ -22,5 +26,28 @@ plot samples gain = do
             e <- peekArray samples dp
             let mags = map ((* gain) . realToFrac . magnitude) e
             let interleave = concatMap (\(x, y) -> [x, y])
-            graphFunc $ interleave $ zip xCoords mags
+            withArray (interleave $ zip xCoords mags) graphFunc 
+
+plotTexture :: Int -> Int -> Int -> Int -> CFloat -> EitherT String IO (Consumer (ForeignPtr (Complex CDouble)) IO ())
+plotTexture width height samples xResolution gain = do
+    graphFunc <- TL.graph width height samples xResolution
+    return $ forever $ do
+        dat <- await
+        lift $ withForeignPtr dat $ \dp -> do
+            e <- peekArray samples dp
+            let mags = map ((* gain) . realToFrac . magnitude) e
+            graphFunc mags
+
+plotWaterfall :: Int -> Int -> Int -> Int -> [GLfloat] -> CFloat -> EitherT String IO (Consumer (ForeignPtr (Complex CDouble)) IO ())
+plotWaterfall windowWidth windowHeight samples height colorMap gain = do
+    graphPipe <- WF.graph windowWidth windowHeight samples height colorMap
+    let process = do
+            dat <- await
+            mags <- lift $ withForeignPtr dat $ \dp -> do
+                e <- peekArray samples dp
+                let mags = map ((* gain) . realToFrac . magnitude) e
+                return mags
+            yield mags
+            process 
+    return $ process >-> graphPipe
 
