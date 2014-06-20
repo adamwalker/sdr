de08b13f2e32baaae4f34dc156e06d193415a34b
Adam Walker <adamwalker10@gmail.com>
2014-06-20 15:09:12 -0400

    ffts take vectors

--- SDR/FFT.hs
+++ SDR/FFT.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE FlexibleContexts #-}
 module SDR.FFT where
 
 import Control.Monad
@@ -12,8 +13,10 @@ import Foreign.Marshal.Array
 import Foreign.ForeignPtr
 
 import Data.Vector.Generic as VG
+import Data.Vector.Generic.Mutable as VGM
 import Data.Vector.Storable as VS
-import Data.Vector.Fusion.Stream.Monadic
+import Data.Vector.Storable.Mutable as VSM
+import Data.Vector.Fusion.Stream as VFS
 
 import Pipes
 import FFTW
@@ -25,7 +28,7 @@ hanning size = VG.generate size func
     where
     func idx = 0.5 * (1 - cos((2 * pi * fromIntegral idx) / (fromIntegral size - 1)))
 
-fftw :: Int -> IO (Pipe (VS.Vector (Complex CDouble)) (VS.Vector (Complex CDouble)) IO ())
+fftw :: (VG.Vector v (Complex CDouble)) => Int -> IO (Pipe (v (Complex CDouble)) (VS.Vector (Complex CDouble)) IO ())
 fftw samples = do
     ina <- mallocForeignBufferAligned samples
     out <- mallocForeignBufferAligned samples
@@ -37,19 +40,20 @@ fftw samples = do
     return $ forever $ do
         out <- lift $ mallocForeignBufferAligned samples
         ina <- lift $ mallocForeignBufferAligned samples
+        let inv = VSM.unsafeFromForeignPtr0 ina samples
 
-        inv <- await
-        let (fp, offset, length) = VS.unsafeToForeignPtr inv
+        inv' <- await
+        lift $ VGM.fill inv $ VFS.mapM return $ VG.stream inv'
+
+        let (fp, offset, length) = VSM.unsafeToForeignPtr inv
 
         lift $ withForeignPtr fp $ \fpp -> 
-            withForeignPtr ina $ \ip -> do
-                copyBytes ip (advancePtr fpp offset) (length * sizeOf (undefined :: (Complex CDouble)))
-                withForeignPtr out $ \op -> 
-                    executeDFT plan ip op
+            withForeignPtr out $ \op -> 
+                executeDFT plan fpp op
 
-        yield $ unsafeFromForeignPtr0 out samples
+        yield $ VS.unsafeFromForeignPtr0 out samples
 
-fftwReal :: Int -> IO (Pipe (VS.Vector CDouble) (VS.Vector (Complex CDouble)) IO ())
+fftwReal :: (VG.Vector v CDouble) => Int -> IO (Pipe (v CDouble) (VS.Vector (Complex CDouble)) IO ())
 fftwReal samples = do
     --Allocate in and out buffers that wont be used because there doesnt seem to be a way to create a plan without them
     ina <- mallocForeignBufferAligned samples
@@ -62,15 +66,15 @@ fftwReal samples = do
     return $ forever $ do
         out <- lift $ mallocForeignBufferAligned ((samples `quot` 2) + 1)
         ina <- lift $ mallocForeignBufferAligned samples
+        let inv = VSM.unsafeFromForeignPtr0 ina samples
 
-        inv <- await
-        let (fp, offset, length) = VS.unsafeToForeignPtr inv
+        inv' <- await
+        lift $ VGM.fill inv $ VFS.mapM return $ VG.stream inv'
+        let (fp, offset, length) = VSM.unsafeToForeignPtr inv
 
         lift $ withForeignPtr fp  $ \fpp -> 
-            withForeignPtr ina $ \ip -> do
-                copyBytes ip (advancePtr fpp offset) (length * sizeOf (undefined :: CDouble))
-                withForeignPtr out $ \op -> 
-                    executeDFTR2C plan ip op
+            withForeignPtr out $ \op -> 
+                executeDFTR2C plan fpp op
 
-        yield $ unsafeFromForeignPtr0 out samples
+        yield $ VS.unsafeFromForeignPtr0 out samples
 
