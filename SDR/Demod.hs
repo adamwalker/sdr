d91595182b9fa23fe68777de9f005550378dbbe9
Adam Walker <adamwalker10@gmail.com>
2014-12-05 19:13:34 +1100

    documentation

--- SDR/Demod.hs
+++ SDR/Demod.hs
@@ -9,6 +9,7 @@ import Pipes
 
 import SDR.Util
 
+-- | FM demodulate a stream of complex samples
 {-# INLINE fmDemodStr #-}
 fmDemodStr :: (RealFloat a) => Complex a -> Stream (Complex a) -> Stream a
 fmDemodStr = mapAccumMV func 
@@ -16,6 +17,7 @@ fmDemodStr = mapAccumMV func
     {-# INLINE func #-}
     func last sample = return (sample, phase (sample * conjugate last))
 
+-- | FM demodulate a vector of complex samples
 {-# INLINE fmDemodVec #-}
 fmDemodVec :: (RealFloat a, Vector v (Complex a), Vector v a) => Complex a -> v (Complex a) -> v a
 fmDemodVec init = unstream . fmDemodStr init . stream
