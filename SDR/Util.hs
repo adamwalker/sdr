3c9a92bca3047b8e179bba1de25026adee712545
Adam Walker <adamwalker10@gmail.com>
2014-12-20 16:49:02 +1300

    better size parsing

--- SDR/Util.hs
+++ SDR/Util.hs
@@ -27,6 +27,7 @@ import qualified Pipes.ByteString as PB
 import Data.Serialize hiding (Done)
 import qualified Data.Serialize as S
 import Options.Applicative
+import Data.Decimal
 
 fork :: Monad m => Producer a m r -> Producer a (Producer a m) r
 fork prod = runEffect $ hoist (lift . lift) prod >-> fork' 
@@ -164,10 +165,10 @@ instance (Num a) => Mult (Complex a) a where
 parseSize :: ReadM Integer
 parseSize = eitherReader $ \arg -> case reads arg of
     [(r, suffix)] -> case suffix of 
-        []  -> return r
-        "K" -> return $ r * 1000 
-        "M" -> return $ r * 1000000
-        "G" -> return $ r * 1000000000
+        []  -> return $ round (r :: Decimal)
+        "K" -> return $ round $ r * 1000 
+        "M" -> return $ round $ r * 1000000
+        "G" -> return $ round $ r * 1000000000
         x   -> Left  $ "Cannot parse suffix: `" ++ x ++ "'"
     _             -> Left $ "Cannot parse value: `" ++ arg ++ "'"
 
