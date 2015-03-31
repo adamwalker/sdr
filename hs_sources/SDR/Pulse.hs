185bca7ecadc7eb61d8ce79c1138b99fd5ff65ba
Adam Walker <adamwalker10@gmail.com>
2015-03-31 15:51:36 +1100

    pulseaudio sink takes floats instead of cfloats

--- hs_sources/SDR/Pulse.hs
+++ hs_sources/SDR/Pulse.hs
@@ -15,7 +15,7 @@ import Pipes
 import Pipes.Concurrent
 
 -- | Returns a consumer that sends all incoming data to pulseaudio. Runs Pulse Audio output writing in a different thread. This is probably what you want as it does not block the entire pipline while the data is being played.
-pulseAudioSink :: IO (Consumer (VS.Vector CFloat) IO ())
+pulseAudioSink :: IO (Consumer (VS.Vector Float) IO ())
 pulseAudioSink = do
     (output, input) <- spawn Single
     doIt <- doPulse 
@@ -23,7 +23,7 @@ pulseAudioSink = do
     return $ toOutput output
 
 -- | Returns a consumer that sends all incoming data to pulseaudio.
-doPulse :: IO (Consumer (VS.Vector CFloat) IO ())
+doPulse :: IO (Consumer (VS.Vector Float) IO ())
 doPulse = do
     s <- simpleNew Nothing "Haskell SDR" Play Nothing "Software Defined Radio library" (SampleSpec (F32 LittleEndian) 48000 1) Nothing Nothing
     return $ for cat $ \buf -> 
