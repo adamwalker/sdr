6eb9ded40eefdd56eaa39da3c659b49663632ca6
Adam Walker <adamwalker10@gmail.com>
2015-04-20 18:27:05 +1000

    some more windows + sinc function

--- hs_sources/SDR/FFT.hs
+++ hs_sources/SDR/FFT.hs
@@ -3,7 +3,10 @@
 {-| Fast FFTs using FFTW -}
 module SDR.FFT (
     -- * Windows
+    sinc,
     hanning,
+    hamming,
+    blackman,
     fftFixup,
 
     -- * FFTs
@@ -35,13 +38,39 @@ mallocForeignBufferAligned elems = do
     ptr <- fftwMalloc $ fromIntegral $ elems * sizeOf (undefined :: a)
     newForeignPtr fftwFreePtr ptr
 
--- | Compute a Hanning window. You probably want to multiply your buffers by one of these before you perform a DFT.
+-- | Compute a sinc function
+sinc :: (Floating n, VG.Vector v n)
+     => Int -- ^ The length. Must be odd.
+     -> n   -- ^ The cutoff frequency (from 0 to 1)
+     -> v n
+sinc size cutoff  = VG.generate size (func . (-) ((size - 1) `quot` 2))
+    where
+    func 0   = 1
+    func idx = sin (pi * cutoff * fromIntegral idx) / (fromIntegral idx * pi)
+
+-- | Compute a Hanning window.
 hanning :: (Floating n, VG.Vector v n) 
         => Int -- ^ The length of the window
         -> v n
 hanning size = VG.generate size func
     where
     func idx = 0.5 * (1 - cos((2 * pi * fromIntegral idx) / (fromIntegral size - 1)))
+  
+-- | Compute a Hamming window. 
+hamming :: (Floating n, VG.Vector v n) 
+        => Int -- ^ The length of the window
+        -> v n
+hamming size = VG.generate size func
+    where
+    func idx = 0.54 - 0.46 * cos((2 * pi * fromIntegral idx) / (fromIntegral size - 1))
+   
+-- | Compute a Blackman window.
+blackman :: (Floating n, VG.Vector v n) 
+        => Int -- ^ The length of the window
+        -> v n
+blackman size = VG.generate size func
+    where
+    func idx = 0.42 - 0.5 * cos((2 * pi * fromIntegral idx) / (fromIntegral size - 1)) - 0.08 * cos((4 * pi * fromIntegral idx) / (fromIntegral size - 1))
 
 -- | Compute a vector of alternating 1s and 0s of the given size.
 fftFixup :: (VG.Vector v n, Num n) 
